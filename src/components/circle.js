70baa2049287f423eca9d3e64423b64031bb27d2
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-26 01:09:49 +0530

    changes done to circle and shape module

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,18 @@
 // import using require
-
+const React = require("react");
+const ReactDOM = require("react-dom");
+const Shape = require("./shape");
 // declare class
-
+class Circle extends Shape {
+  constructor(props) {
+    super(props);
+  }
+  calculateArea() {
+    return "overRide the shape module Calculate Area";
+  }
+}
 
 // export class using module.exports
+module.exports = {
+  class: Circle
+};
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
 // declare class
 
+class Shape {
+  constructor(props) {
+    color: props;
+  }
+  calculateArea() {
+    return "overRide the shape module Calculate Area";
+  }
+  drawShape() {
+    return "shape draw";
+  }
+}
+
+// export class using module.exports
+module.exports = {
+  class: Shape
+};
+
 // export class using module.exports
